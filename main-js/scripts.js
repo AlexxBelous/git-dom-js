1d3568a10c329cb773a88df6080b4b849fbadd83
AlexxBelous <connectionwithme1@gmail.com>
2023-01-07 11:25:11 +0200

    fix

--- main-js/scripts.js
+++ main-js/scripts.js
@@ -1,33 +1,3 @@
-/*******************************************
-  При клике на черный квадрат,
-  который находится на странице(элемент div c 
-  классом square) необходимо добавлять еще одни 
-  класс с именем green
- *******************************************/
-
-
-
-
-
-
-
-
-
-/*******************************************
-  При клике на зеленый круг,
-  который находится на странице(элемент div c 
-  классом square-green) необходимо удалить второй 
-  класс с именем green.
- *******************************************/
-
-
-
-
-
-
-
-
-
 
   /*******************************************
   При клике на желтый треугольник, который находится 
@@ -103,58 +73,15 @@
 
 
 
+  // let triangle = document.querySelector('.triangle');
 
-  /*******************************************
-  При клике на черный квадрат,
-  который находится на странице(элемент div c 
-  классом square) необходимо добавлять еще одни 
-  класс с именем green
- *******************************************/
-
-// let square = document.querySelector('.square');
-
-// square.addEventListener('click', function() {
-//     square.classList.add('green');
-// })
-
-
-
-/*******************************************
-  При клике на зеленый круг,
-  который находится на странице(элемент div c 
-  классом square-green) необходимо удалить второй 
-  класс с именем green.
- *******************************************/
-
-
-//   let circle = document.querySelector('.circle');
-
-//   circle.addEventListener('click', function() {
-//       circle.classList.remove('green');
-//   })
-
-
-
-
-  /*******************************************
-  При клике на желтый треугольник, который находится 
-  на странице(элемент div c классом triangle)
-  необходимо проверить существует ли в блоке класс 
-  yellow и если существует, то необходимо его удалить 
-  и на его место добавить второй класс с именем green.
-  ******************************************/
-
-
-
-//   let triangle = document.querySelector('.triangle');
-
-//   triangle.addEventListener('click', function() {
-//     if (triangle.classList.contains('yellow')) {
-//         triangle.classList.add('green')
-//         triangle.classList.remove('yellow')
-//     }
-//     else {
-//         triangle.classList.add('yellow')
-//         triangle.classList.remove('green');
-//     }
-//   })
+  // triangle.addEventListener('click', function() {
+  //   if (triangle.classList.contains('yellow')) {
+  //       triangle.classList.add('green')
+  //       triangle.classList.remove('yellow')
+  //   }
+  //   else {
+  //       triangle.classList.add('yellow')
+  //       triangle.classList.remove('green');
+  //   }
+  // })
