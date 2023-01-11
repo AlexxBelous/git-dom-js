a2d98a543ecc14d83c7313ffa459aafc69315b2f
Alex <connectionwithme1@gmail.com>
2023-01-11 12:50:34 +0200

    fix question

--- main-js/scripts.js
+++ main-js/scripts.js
@@ -10,10 +10,74 @@
 
 
 
-const list = document.querySelector('.list');
 
-list.addEventListener('click', (event) => {
-  const target = event.target
-  console.log(target);
-  target.classList.toggle('red');
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const list = document.querySelector('.list');
+
+// list.addEventListener('click', (event) => {
+//   const target = event.target
+//   console.log(target);
+//   target.classList.toggle('red');
+// })
